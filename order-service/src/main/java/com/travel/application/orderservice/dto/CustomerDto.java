d47e73ce3a39877d47469ba16404ffcfb7a0c4af
Savel-cmyk <savelikor@gmail.com>
2025-02-24 21:26:11 +0300

    REFACTORED CustomerDto.java validation of email and phone number by deleting files with excessive code

--- order-service/src/main/java/com/travel/application/orderservice/dto/CustomerDto.java
+++ order-service/src/main/java/com/travel/application/orderservice/dto/CustomerDto.java
@@ -1,10 +1,21 @@
 package com.travel.application.orderservice.dto;
 
-import com.travel.application.orderservice.annotation.ValidateEmailFormat;
-import com.travel.application.orderservice.annotation.ValidatePhoneFormat;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Pattern;
 
+/**
+ * @param surname
+ * @param name
+ * @param patronymic
+ * @param email <i><b>test cases:</b></i><br/>
+ * <i>0r.4cu.5t@gmail.mail.com - <b>true</b>,</i><br/>
+ * <i>gu@rubic.ru - <b>true</b>,</i><br/>
+ * <i>13@out.1 - <b>false</b></i>
+ * @param phoneNumber
+ * @author Savel-cmyk
+ * @version 0.0.1
+ */
 public record CustomerDto(
 
         @NotBlank
@@ -13,10 +24,17 @@ public record CustomerDto(
         String name,
         @NotEmpty
         String patronymic,
-        @ValidateEmailFormat
+        @Pattern(
+                regexp = "^([0-9]+[a-zA-Z]+|[a-zA-Z]+[a-zA-Z0-9]*)([.]([0-9]+[a-zA-Z]+|[a-zA-Z]+[a-zA-Z0-9]*)){0,2}" +
+                        "@[a-zA-Z]+([.][a-zA-Z]+)?[.][a-zA-Z]{2,}$",
+                message = "Email format is not valid"
+        )
         @NotEmpty
         String email,
-        @ValidatePhoneFormat
+        @Pattern(
+                regexp = "^[0-9]{7,10}$",
+                message = "Phone number format is not valid"
+        )
         @NotEmpty
         String phoneNumber
 ) {
