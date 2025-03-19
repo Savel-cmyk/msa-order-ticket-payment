cc7945c3dcfec40ec0fcfe255754f0cef2b4c462
Savel-cmyk <savelikor@gmail.com>
2025-03-19 05:14:36 +0300

    UPDATED account service's security configuration for user's role retrieval from jwt

--- account-service/src/main/java/com/travel/application/accountservice/config/SecurityConfig.java
+++ account-service/src/main/java/com/travel/application/accountservice/config/SecurityConfig.java
@@ -1,16 +1,21 @@
 package com.travel.application.accountservice.config;
 
+import com.travel.application.accountservice.util.JWTAuthConverter;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
+@RequiredArgsConstructor
+@EnableMethodSecurity
 public class SecurityConfig {
 
+    private final JWTAuthConverter jwtAuthConverter;
     private final String[] freeResourceUrls = {"/eureka/**", "/actuator/**"};
 
     @Bean
@@ -22,7 +27,9 @@ public class SecurityConfig {
                         .anyRequest()
                         .authenticated()
                 )
-                .oauth2ResourceServer((oauth2) -> oauth2.jwt(Customizer.withDefaults()));
+                .oauth2ResourceServer(oauth2 -> oauth2.jwt(
+                        jwtConfigurer -> jwtConfigurer.jwtAuthenticationConverter(jwtAuthConverter)
+                ));
 
         return http.build();
     }
